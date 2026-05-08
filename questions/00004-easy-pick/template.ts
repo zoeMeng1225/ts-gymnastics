7adbc6c950650bfa30d6af423c40a79faf8866fe
zoeMeng1225 <zoebo1225@gmail.com>
2026-05-07 22:15:59 -0700

    feat(easy): solve 00004-easy-pick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,6 @@
-type MyPick<T, K> = any
+// 1.K extends keyof T ensures K is a valid key of T
+// [P in k] interates over the union type K
+// T[P] gets the value type of the correesponding key
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
