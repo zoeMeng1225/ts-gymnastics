67570a37c76ea8bf0066dd9e613ee0735813ec29
zoeMeng1225 <zoebo1225@gmail.com>
2026-05-07 22:29:20 -0700

    feat(easy): solve 00007-easy-readonly

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,9 @@
-type MyReadonly<T> = any
+// implement a custom readonly utility type to make all properties in T unassignable
+
+// 1.interate over all keys of T using 'keyof T' and 'in'
+// 2.add the readonly modifier before the key
+// 3. keep the originak value type T[P]
+
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P]
+}
