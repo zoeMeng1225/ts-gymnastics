b009defc6e34996a1c0eff658a59055fa3bc9cf4
zoeMeng1225 <zoebo1225@gmail.com>
2026-05-10 21:55:01 -0700

    feat(easy): solve 00189-awaited using conditional types and recursion

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,14 @@
-type MyAwaited<T> = any
+// implement a cutom awaited utility type to unwrap Promise types
+
+// restrict T to be a Promise-like type(having a 'then' method)
+
+type MyAwaited<T extends PromiseLike<any>> =
+// use infer U to extract the inner resolved type
+T extends PromiseLike<infer U> ? U extends PromiseLike<any> ? MyAwaited<U> : U
+// fallback if T is not a Promise(should not be reached due to contraint):
+  : never
+
+// about use infer:  T extends Promise<infer U> ? U : never is work, but for actual business,
+// it may run into nested Promise, like Promise<Promise<string>>.
+
+// PromiseLike: more in line with real-world engineering environments
