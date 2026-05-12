eff821647a8eaa2ac2b5697eaf920e7a006ba737
zoeMeng1225 <zoebo1225@gmail.com>
2026-05-11 20:32:32 -0700

    feat(easy): solve 00268-if

--- questions/00268-easy-if/template.ts
+++ questions/00268-easy-if/template.ts
@@ -1,1 +1,6 @@
-type If<C, T, F> = any
+// Implement the util type If<C,T,F>
+
+// C extends boolean, strictly cntrains C to only accept true or false
+// C extends true ? T : F bahaves exactly like a ternary operator
+
+type If<C extends boolean, T, F> = C extends true ? T : F
